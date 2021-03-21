7bf4a0092b4cad315b3ebf8b8c72ee06604be25c
pakio <pakio@users.noreply.github.com>
2021-03-21 07:07:21 +0000

    Google Java Format

--- src/main/java/com/pakio/kuromojiapi/KuromojiApiApplication.java
+++ src/main/java/com/pakio/kuromojiapi/KuromojiApiApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KuromojiApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KuromojiApiApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(KuromojiApiApplication.class, args);
+  }
 }
--- src/main/java/com/pakio/kuromojiapi/service/KuromojiService.java
+++ src/main/java/com/pakio/kuromojiapi/service/KuromojiService.java
@@ -7,7 +7,6 @@ import com.atilika.kuromoji.ipadic.Tokenizer;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
@@ -23,14 +22,11 @@ public class KuromojiService {
     tokenizerHashMap.put(Mode.EXTENDED, new Tokenizer.Builder().mode(Mode.EXTENDED).build());
   }
 
-  public Map<Integer, String> parseWithKuromoji(String text, Mode mode){
+  public Map<Integer, String> parseWithKuromoji(String text, Mode mode) {
     List<Token> tokens = tokenizerHashMap.get(mode).tokenize(text);
 
     AtomicInteger i = new AtomicInteger();
     return tokens.stream()
-        .collect(Collectors.toMap(
-            s -> i.getAndIncrement(),
-            TokenBase::getSurface
-        ));
+        .collect(Collectors.toMap(s -> i.getAndIncrement(), TokenBase::getSurface));
   }
 }
